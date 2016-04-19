d56195985c3469ae2dea706f5c52a3bdfb3f1974
Jérémy Buget <jbuget@octo.com>
2016-04-20 00:47:14 +0200

    Add tests for People controller

--- test/controllers/people_test.js
+++ test/controllers/people_test.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const Proxyquire = require('proxyquire');
-
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const describe = lab.describe;
 const it = lab.it;
-const before = lab.before;
 const afterEach = lab.afterEach;
 
 const Sinon = require('sinon');
@@ -14,9 +11,45 @@ const Chai = require('chai');
 Chai.should();
 Chai.use(require('sinon-chai'));
 
-const Boom = require('boom');
-
-const Project = require('../../app/models/project');
+const Person = require('../../app/models/person');
 
 const Server = require('../../app/server');
 
+describe('Projects API', () => {
+
+    describe('GET /people', () => {
+
+        afterEach((done) => {
+
+            Person.list.restore();
+            done();
+        });
+
+        it('should return all the projects', (done) => {
+            // given
+            const people = [
+                { nickname: 'Batman', firstName: 'Bruce', lastName: 'Wayne' },
+                { nickname: 'Superman', firstName: 'Clark', lastName: 'Kent' },
+                { nickname: 'Wonder woman', firstName: 'Diana', lastName: 'Prince' }
+            ];
+            Sinon.stub(Person, 'list', (callback) => {
+
+                callback(null, people);
+            });
+
+            //when
+            Server.inject({ method: 'GET', url: '/people' }, (response) => {
+
+                // then
+                response.statusCode.should.equal(200);
+                response.result.should.equal(people);
+                done();
+            });
+        });
+    });
+
+    describe('POST /people', () => {
+
+    });
+
+});
--- test/controllers/projects_test.js
+++ test/controllers/projects_test.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const Proxyquire = require('proxyquire');
-
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const describe = lab.describe;
 const it = lab.it;
-const before = lab.before;
 const afterEach = lab.afterEach;
 
 const Sinon = require('sinon');
@@ -14,7 +11,6 @@ const Chai = require('chai');
 Chai.should();
 Chai.use(require('sinon-chai'));
 
-const Boom = require('boom');
 const Project = require('../../app/models/project');
 
 const Server = require('../../app/server');
