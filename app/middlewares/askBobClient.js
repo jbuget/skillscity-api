560a8a3aaccd96974939cff6b33ada819be2ffb5
Jérémy Buget <jbuget@octo.com>
2016-04-27 23:05:32 +0200

    Move AskBob endpoint into config files

--- app/middlewares/askBobClient.js
+++ app/middlewares/askBobClient.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const Request = require('request');
+const Config = require('config');
 
 module.exports.fetchPeople = function () {
 
     return new Promise((resolve, reject) => {
 
-        const endpoint = 'http://askbob.octo.com/api/v1/vqZe12GQsvPvQUNSjW5xceiKh/people.json';
-        Request.get({ url: endpoint, json: true }, (err, response, body) => {
+        Request.get({ url: Config.get('askbob.endpoint'), json: true }, (err, response, body) => {
 
             if (err || response.statusCode !== 200) {
                 return reject(err);
