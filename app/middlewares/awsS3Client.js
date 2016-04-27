73edee8a79831204b941432b6c7be19cc18f0565
Jérémy Buget <jbuget@octo.com>
2016-04-27 23:50:32 +0200

    Update library versions and remove useless tools (such as s3-stream-uploader)

--- app/middlewares/awsS3Client.js
+++ app/middlewares/awsS3Client.js
@@ -1,33 +1,26 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const s3Stream = require('s3-upload-stream')(new AWS.S3());
 const Config = require('config');
-const ONE_MB = 1048576;
 
 module.exports.uploadStreamToAwsS3 = function (stream, key, contentType) {
 
     return new Promise((resolve, reject) => {
 
-        const upload = s3Stream.upload({
-            Bucket: Config.get('s3.bucket'),
-            Key: key,
+        const s3 = new AWS.S3();
+        const params = {
             ACL: 'public-read',
-            StorageClass: 'REDUCED_REDUNDANCY',
-            ContentType: contentType
+            Body: stream,
+            Bucket: Config.get('s3.bucket'),
+            ContentType: contentType,
+            Key: key
+        };
+        s3.upload(params, (err, data) => {
+
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
         });
-
-        upload.maxPartSize(ONE_MB);
-
-        upload.on('error', (err) => {
-
-            return reject(err);
-        });
-
-        upload.on('uploaded', (details) => {
-
-            return resolve(details);
-        });
-        stream.pipe(upload);
     });
 };
